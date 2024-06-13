8e170652008fe51a6b643f83085b0f0a994803bc
Joshuamomanyi <joshuamomanyi2323@gmail.com>
2024-06-13 21:31:23 +0300

    Made changes and linked all data to relevant django files

--- Joshapp/urls.py
+++ Joshapp/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Root URL
-    path('home/', views.index, name='home'),  # Home URL
-    path('index/', views.home, name='index'),  # Home URL
-
+    path('', views.product_list, name='product_list'),  # Home URL
 
 ]
--- Joshapp/views.py
+++ Joshapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Product
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h1 style= 'color:brown' >My first webpage with Django</h1>")
+#def index(request):
+ #   return HttpResponse("<h1 style= 'color:brown' >My first webpage with Django</h1>")
 
-def home(request):
-    return HttpResponse("<h1 style= 'color:brown' >My Recap</h1>")
-
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+   'products': products,
+    }
+    return render(request, 'Joshapp/product_list.html', context )
 
 
 
